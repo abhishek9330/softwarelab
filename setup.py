95e4a24f5c49b8ac3efc1118c2d39e3936cdd21a
Abhishek Choudhary <abhishekchoudhary9330@gmail.com>
2024-10-29 20:29:44 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="softwarelab",  # Replace with your library's name
+    version="1.0.0",
+    author="Abhishek Choudhary",
+    author_email="abhishek.choudhary@iitb.ac.in",
+    description="A library to create webapps with ease",
+    url="https://github.com/abhishek9330/softwarelab",
+    packages=find_packages(),  # Automatically finds and includes packages
+    install_requires=[  # List any dependencies
+        # e.g., 'numpy', 'pandas'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Update if using a different license
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",  # Define the Python version requirement
+)
